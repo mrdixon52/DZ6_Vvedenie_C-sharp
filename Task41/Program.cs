7aeecf67c44799405d5d3b7dd5a40e7cbd5b566b
Arseny Zhalnin <mrzhalnin@gmail.com>
2022-12-11 19:39:44 +0300

    add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Clear();
+Console.WriteLine("Enter 0 to stop input");
+int n = Convert.ToInt32(Console.ReadLine());
+int positive = 0;
+if (n > 0)
+{
+    positive++;
+}
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0)
+    {
+        positive++;
+    }
+    
+}
+Console.WriteLine("End");
+Console.WriteLine($"Count more than 0 = {positive}");
